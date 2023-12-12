2c1c9e5c923468a180dea912f776c8dd0b58e0a0
Khizyr591 <khizer.abdulsalam@gmail.com>
2023-12-12 21:09:58 +0500

    update

--- herradura-villas/src/components/Hero.jsx
+++ herradura-villas/src/components/Hero.jsx
@@ -3,14 +3,15 @@ import styles from '../style'
 
 export const Hero = () => (
     <div className="">
-        <div className="max-w-3xl mx-auto">
+        <div className="">
             <h1 className={`${styles.heading1} flex items-center`}>
                 Elevate Your Portfolio with Prime Multifamily Space
             </h1>
             <p className={`${styles.paragraph} flex items-center`}>
-                Situated in an area of revitalization, Herradura Villas is surrounded by new developments and residential remodel projects, making it a prime location for investment.
+                Situated in an area of revitalization, Herradura Villas
+                is surrounded by new developments and residential remodel
+                projects, making it a prime location for investment.
             </p>
-
         </div>
     </div>
 )
--- herradura-villas/src/components/Navbar.jsx
+++ herradura-villas/src/components/Navbar.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import { navLinks } from '../constants'
-import {logo} from '../assets'
+import { logo } from '../assets'
 import { Link, useLocation } from 'react-router-dom';
 
 export const Navbar = () => (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
-        <a href="/"><img src={logo} alt="Herradura Vills" className="xs:w-[255px] w-44 h-auto object-contain" /></a>
-
-        <ul className="justify-end items-center flex-1">
+    <nav className="">
+        <a href="/">
+            <img src={logo} alt="Herradura Vills" className="" />
+        </a>
+        <ul className="">
             {navLinks.map((nav) => (
-                <li
-                    key={nav.id}
-                    className={`font-poppins font-normal cursor-pointer text-[16px] md:text-[18px]`}
-                >
+                <li key={nav.id}>
                     <Link to={nav.path}>{nav.title}</Link>
                 </li>
             ))}
