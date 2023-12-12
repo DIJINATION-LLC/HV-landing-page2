97ac1234a703ea37020c9074cf7b18ddd051f315
Khizyr591 <khizer.abdulsalam@gmail.com>
2023-12-12 22:10:43 +0500

    Update FAQ.jsx

--- herradura-villas/src/components/FAQ.jsx
+++ herradura-villas/src/components/FAQ.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 import { faq } from '../constants'
 import styles from '../style'
-import { upArrow,downArrow } from '../assets';
+import { upArrow, downArrow } from '../assets';
 
 const FAQItem = ({ title, description, isOpen, toggleItem }) => {
     return (
         <div className=''>
             <div className=''>
                 <h5 className={`${styles.heading5} text-black`}>{title}</h5>
-                <img className={`faq-icon ${isOpen ? 'open': ''}`}
-                src={isOpen ? downArrow : upArrow}
-                alt="Dropdown Icon"/>
+                <img className={`faq-icon ${isOpen ? 'open' : ''}`}
+                    src={isOpen ? downArrow : upArrow}
+                    alt="Dropdown Icon" />
             </div>
-            <div className={`faq-description ${isOpen ? 'open': ''}`}>
-
+            <div className={`faq-description ${isOpen ? 'open' : ''}`} style={{ maxHeight: isOpen ? '1000px' : '0' }}>
+                <p className={`${styles.paragraph} p-2 text-black`}>{description}</p>
             </div>
         </div>
     );
