e10a44bc3ebaa40c1cef4cc8908d177e709a0e0f
Khizyr591 <khizer.abdulsalam@gmail.com>
2023-12-12 21:28:44 +0500

    Update

--- herradura-villas/src/components/CTA.jsx
+++ herradura-villas/src/components/CTA.jsx
@@ -4,9 +4,13 @@ import styles from '../style'
 export const CTA = () => (
     <div className="bg-yaleblue justify-center align-middle">
         <div className="">
-            <h2 className={`${styles.heading2} text-white`}>Login to Secure Data-vault</h2>
-            <p className={`${styles.paragrah} text-white`}>Our Secure Data-vault contains all legal and financial information necessary for you to make an informed decision.</p>
-           
+            <h2 className={`${styles.heading2} text-white`}>
+                Login to Secure Data-vault
+            </h2>
+            <p className={`${styles.paragrah} text-white`}>
+                Our Secure Data-vault contains all legal and financial information necessary for you to make an informed decision.
+            </p>
+
         </div>
     </div>
 )
