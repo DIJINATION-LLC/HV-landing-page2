c3ae6046eb6cb0f4e245215416c567eebc41a642
Taimur Israr <taimurisrar806@gmail.com>
2023-12-13 23:43:21 +0500

    Update Hero.jsx

--- herradura-villas/src/components/Hero.jsx
+++ herradura-villas/src/components/Hero.jsx
@@ -9,16 +9,34 @@ export const Hero = () => {
     const sliderSetting = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
-        customPaging: (i) => ()
+        autoplay: true,
+        autoplaySpeed: 9000,
+        arrows: false,
+        appendDots: (dots) => (
+          <div>
+            <ul>{dots}</ul>
+          </div>
+        ),
+        customPaging: (i) => (
+          <div
+            style={{
+              width: '12px',
+              height: '12px',
+              borderRadius: '50%',
+              margin: '0 5px',
+              background: i === currentSlide ? '#5cae17' : '#d9d9d9',
+            }}
+          />
+        ),
         beforeChange: (current, next) => setCurrentSlide(next),
     };
     const heroSlides = slides.map((slide, index) => (
         <div className={`${styles.boxWidth} flex flex-col gap-[30px]`}>
             <h1 className={`${styles.heading1} w-[600px]`}>
-                Elevate Your Portfolio with Prime Multifamily Space
+                slides
             </h1>
             <p className={`${styles.paragraph} text-white w-[600px]`}>
                 Situated in an area of revitalization, Herradura Villas is surrounded by new developments and residential remodel projects, making it a prime location for investment.
