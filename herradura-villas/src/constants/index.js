cb6c891bb19a1c2c768b28dfd8d62f5977d3a3c9
Taimur Israr <taimurisrar806@gmail.com>
2023-12-15 20:02:59 +0500

    Update index.js

--- herradura-villas/src/constants/index.js
+++ herradura-villas/src/constants/index.js
@@ -48,18 +48,7 @@ export const gallery = [
         alt: 'herradura villas',
         cation: 'herradura villas',
         image: gallery1,
-    },
-    {
-        id: 'gallery-4',
-        alt: 'herradura villas',
-        cation: 'herradura villas',
-        image: gallery1,
-    },{
-        id: 'gallery-5',
-        alt: 'herradura villas',
-        cation: 'herradura villas',
-        image: gallery1,
-    },
+    }
 ];
 export const faq = [
     {
