1823e2fdc86b765ef47e6398ccf13091fb50ab4c
Taimur Israr <taimurisrar806@gmail.com>
2023-12-18 17:57:56 +0500

    Final push

--- herradura-villas/src/components/Gallery.jsx
+++ herradura-villas/src/components/Gallery.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import styles from '../style';
 import { gallery } from '../constants';
-import Lightbox from 'react-images';
+import ModalImage from 'react-modal-image';
 
 export const Gallery = () => {
     const [lightboxOpen, setLightboxOpen] = useState(false);
-    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+    const [selectedImage, setSelectedImage] = useState('');
 
     const settings = {
         dots: false,
@@ -21,29 +21,24 @@ export const Gallery = () => {
         pauseOnHover: true,
         responsive: [
             {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                },
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+              },
             },
             {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                },
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              },
             },
         ],
     };
 
-    const openLightbox = (index) => {
-        setSelectedImageIndex(index);
+    const openLightbox = (imageSrc) => {
+        setSelectedImage(imageSrc);
         setLightboxOpen(true);
     };
-
-    const imagesForLightbox = gallery.map((item) => ({
-        src: item.image,
-        alt: item.alt,
-    }));
 
     return (
         <section id='gallery' className={`${styles.flexCenter} w-full py-[50px]`}>
@@ -51,20 +46,18 @@ export const Gallery = () => {
                 <h2 className={`${styles.heading2} text-yaleblue`}>Gallery</h2>
                 <div className="w-full">
                     <Slider {...settings}>
-                        {gallery.map((item, index) => (
-                            <div key={item.id} className='p-2' onClick={() => openLightbox(index)}>
+                        {gallery.map((item) => (
+                            <div key={item.id} className='p-2' onClick={() => openLightbox(item.image)}>
                                 <img src={item.image} alt={item.alt} className='slider-image' />
                             </div>
                         ))}
                     </Slider>
                     {lightboxOpen && (
-                        <Lightbox
-                            images={imagesForLightbox}
-                            isOpen={lightboxOpen}
+                        <ModalImage
+                            small={selectedImage}
+                            large={selectedImage}
+                            alt="Gallery Image"
                             onClose={() => setLightboxOpen(false)}
-                            currentImage={selectedImageIndex}
-                            onClickPrev={() => setSelectedImageIndex((prev) => prev - 1)}
-                            onClickNext={() => setSelectedImageIndex((prev) => prev + 1)}
                         />
                     )}
                 </div>
